431d21e16f499e8c5b5c6b187a3447ae39995b7c
mguerra <msg4@poli.br>
2022-08-01 19:57:01 -0300

    Adicionado método que preenche matriz das peças!

--- repository/LPOO_PUZZLEN/src/Jogo/Tabuleiro.java
+++ repository/LPOO_PUZZLEN/src/Jogo/Tabuleiro.java
@@ -2,15 +2,31 @@ package Jogo;
 
 public class Tabuleiro {
 	private int matriz;
-	private int peca;
-	private int[] listaPecas;
+	private String[][] listaPecas;
+	private int contadorMatriz;
 	
 	public Tabuleiro(int matriz){
-		this.matriz = matriz;
+		setMatriz(matriz);
 	}
 	
-	public void geraMatriz() {}
+	public void setMatriz(int matriz) { //armazena o valor da matriz
+		if(matriz == 3 || matriz == 4) {
+			this.matriz = matriz;
+		}
+	}
 	
-	public void preencheMatriz() {}
+	
+	public void preencheArray() { //preenche os numeros da matriz
+		String[][] tempArray = new String[matriz + 1][matriz + 1];
+		this.contadorMatriz = 0;
+		for(int i = 0; i <= matriz; i++) {
+			for(int j = 0; j < matriz; j++) {
+				tempArray[i][j] ="[" + String.valueOf(contadorMatriz + 1) + "]";
+				contadorMatriz++;
+			}
+		}
+		tempArray[matriz - 1][matriz - 1] = "[ ]";
+		this.listaPecas = tempArray;
+	}
 	
 }
---
+++ repository/LPOO_PUZZLEN/src/Jogo/Testando.java
@@ -0,0 +1,9 @@
+package Jogo;
+
+public class Testando {
+	public static void main(String[] args){
+		Tabuleiro tab = new Tabuleiro(3);
+		tab.preencheArray();
+	}
+	
+}
